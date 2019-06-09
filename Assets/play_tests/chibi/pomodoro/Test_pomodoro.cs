80ecd303ff2a3e752d1fb2f5752d771ab1b56524
Dem4ply <dem4ply@users.noreply.github.com>
2019-06-08 20:54:58 -0500

    pomodors listos

--- Assets/play_tests/chibi/pomodoro/Test_pomodoro.cs
+++ Assets/play_tests/chibi/pomodoro/Test_pomodoro.cs
@@ -19,7 +19,7 @@ namespace tests.pomodoro
 
 		public override void Instanciate_scenary()
 		{
-			// base.Instanciate_scenary();
+			base.Instanciate_scenary();
 		}
 
 		[UnityTest]
@@ -42,6 +42,29 @@ namespace tests.pomodoro
 			yield return new WaitForSeconds( 0.1f );
 			pomodoro.tick( 10f );
 			Assert.AreEqual( 10f, pomodoro._sigma_frecuency );
+			Assert.IsTrue( pomodoro.is_time );
+		}
+
+		[UnityTest]
+		public IEnumerator should_create_a_container_for_the_ticker()
+		{
+			var instance = singleton.pomodoro.Pomodoro_singleton.instance;
+			yield return new WaitForSeconds( 1f );
+			var container = helper.game_object.Find.all( instance.container_name )[0];
+			Assert.IsTrue( container );
+			var ticker = container.GetComponent<Pomodoro_ticker>();
+			Assert.IsTrue( ticker );
+		}
+
+		[UnityTest]
+		public IEnumerator ticker_shoudl_add_time_with_the_time()
+		{
+			var instance = singleton.pomodoro.Pomodoro_singleton.instance;
+			Pomodoro pomodoro = Pomodoro.CreateInstance<Pomodoro>();
+			pomodoro.add_to_global();
+			yield return new WaitForSeconds( 1.1f );
+
+			Assert.Greater( pomodoro._sigma_frecuency, 1 );
 			Assert.IsTrue( pomodoro.is_time );
 		}
 	}
