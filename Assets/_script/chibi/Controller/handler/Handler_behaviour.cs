5358e63555cdf81e93e3402804e31757cee7af85
Dem4ply <dem4ply@users.noreply.github.com>
2019-07-21 23:16:20 -0500

    handler para el control

---
+++ Assets/_script/chibi/Controller/handler/Handler_behaviour.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+
+namespace chibi.controller.handler
+{
+	public class Handler_behaviour: Chibi_behaviour
+	{
+		public List<Handler> handlers = new List<Handler>();
+		public bool is_global = true;
+
+		private void OnTriggerEnter( Collider other )
+		{
+			var controller = other.transform.GetComponent< Controller >();
+			if ( controller && ( is_global ) )
+			{
+				foreach ( var handler in handlers )
+					handler.action( controller );
+			}
+		}
+
+		public bool is_in_affected_controllers( Controller controller )
+		{
+			return false;
+		}
+	}
+}
