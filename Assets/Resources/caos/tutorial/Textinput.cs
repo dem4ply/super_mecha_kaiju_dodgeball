6b1084b4bbf67c511894ca86b14644e7e4e47488
theurgistew3 <theurgistew3@users.noreply.github.com>
2020-06-04 12:44:19 -0600

    botones y mi intento

---
+++ Assets/Resources/caos/tutorial/Textinput.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Security.Cryptography.X509Certificates;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Textinput : MonoBehaviour
+{
+    InputField input;
+    InputField.SubmitEvent se;
+    public Textinput output;
+
+
+
+
+    void Start()
+    {
+        input = gameObject.GetComponent<InputField>();
+        se = new InputField.SubmitEvent();
+        se.AddListener(SubmitInput);
+        input.onEndEdit = se;
+
+    }
+
+    private void SubmitInput(string arg0)
+    {
+        string currentText = Output.text; //maybe add ToString()?
+        string newText = currentText + "/n" + arg0;
+        Output.text = newText;
+        input.ActivateInputField();
+    }
+}
+
