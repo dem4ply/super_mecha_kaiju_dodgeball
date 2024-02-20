28b7efe4b05acded7846cdae34814f030e708906
dem4ply <dem4ply@gmail.com>
2024-02-20 14:03:36 -0600

    preparacion para selecionar celda con el mouse

--- Assets/Resources/metroidvania/scripts/controller/Metroidvania_player_controller.cs
+++ Assets/Resources/metroidvania/scripts/controller/Metroidvania_player_controller.cs
@@ -11,11 +11,25 @@ namespace metroidvania.controller.player
 	{
 		public Controller_metroidvania_npc player;
 
+		public Camera main_camera;
+
 		public virtual Vector2 mouse_position
 		{
+			get {
+				throw new System.NotImplementedException();
+			}
 			set {
 				player.mouse_position = value;
 			}
+		}
+
+		protected override void _init_cache()
+		{
+			base._init_cache();
+			if ( !main_camera )
+				main_camera = helper.game_object.camera.maid_camera;
+			if ( !main_camera )
+				debug.error( "no se asigno la camara al control" );
 		}
 	}
 }
--- Assets/Resources/metroidvania/scripts/controller/player/Metroidvania_player_ui_controller.cs
+++ Assets/Resources/metroidvania/scripts/controller/player/Metroidvania_player_ui_controller.cs
@@ -11,6 +11,20 @@ namespace metroidvania.controller.player
 	{
 		public metroidvania.grid.Grid_ui grid_ui;
 
+		public Transform debug_shit;
+
+		protected Vector2 _mouse_axis;
+
+		public override Vector2 mouse_position
+		{
+			get {
+				return _mouse_axis;
+			}
+			set {
+				_mouse_axis = value;
+			}
+		}
+
 		protected override void _init_cache()
 		{
 			base._init_cache();
@@ -23,10 +37,28 @@ namespace metroidvania.controller.player
 			base.action( name, e );
 			switch ( name )
 			{
+				case "fire":
+					mouse_position_to_cell( this.mouse_position );
+					break;
 				default:
 					debug_action( name, e );
 					break;
 			}
+		}
+
+		protected Vector3 mouse_position_to_cell( Vector2 mouse_position )
+		{
+			var distancia = main_camera.transform.InverseTransformPoint( transform.position );
+			Vector3 mouse_3d = new Vector3( mouse_position.x, mouse_position.y, distancia.z );
+			Vector3 world_position = main_camera.ScreenToWorldPoint( mouse_3d );
+
+			debug_shit.position = world_position;
+
+			int x, y;
+			grid_ui.grid.get_x_y_from_world( world_position, out x, out y );
+			debug.log( "x: {0}, {1}", x, y );
+
+			return world_position;
 		}
 	}
 }
