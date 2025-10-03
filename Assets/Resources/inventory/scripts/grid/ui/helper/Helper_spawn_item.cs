ddc327d6ebb1cf4352ec10cc9929291bce28d3db
dem4ply <dem4ply@gmail.com>
2025-10-03 02:51:04 -0600

    se remueven propiedades inutiles

--- Assets/Resources/inventory/scripts/grid/ui/helper/Helper_spawn_item.cs
+++ Assets/Resources/inventory/scripts/grid/ui/helper/Helper_spawn_item.cs
@@ -11,9 +11,6 @@ namespace inventory.ui.grid.test_helper
 {
 	public class Helper_spawn_item: chibi.Chibi_ui
 	{
-		public GameObject item;
-		public inventory.ui.grid.item.Item_ui_grid item_ui;
-
 		public inventory.item.Item_grid item_obj;
 		public inventory.ui.grid.Grid_ui inventory_grid;
 
